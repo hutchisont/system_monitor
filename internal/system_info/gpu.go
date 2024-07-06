99bebaa28e41871ee42921b915f7693c20faefd2
hutchisont <24536068+hutchisont@users.noreply.github.com>
2024-07-06 02:23:47 -0500

    start setting up gpu stuff
    doesn't do anything useful yet, still need to figure
    out how to get the data

--- internal/system_info/gpu.go
+++ internal/system_info/gpu.go
@@ -1,5 +1,7 @@
 package SystemInfo
 
+import "fmt"
+
 // TODO(Tyler): This will have to figure out the pci device number of the GPU
 // and then read from /sys/bus/pci/devices/<id>/
 // I don't know what it exactly needs to read yet but some likely ones will be
@@ -7,3 +9,33 @@ package SystemInfo
 // mem_info_vram_total
 // mem_info_vram_used
 // gpu_busy_percent
+//
+// would be really nice to get name as well, will have to figure out where to
+// get that from
+
+const pciDevicesBasePath = "/sys/bus/pci/devices/"
+
+type GPU struct {
+	gpuPCIDeviceID  string
+	gpuBusyPercent  float64
+	vramBusyPercent float64
+	vramTotal       float64
+	vramUsed        float64
+	vramAvailable   float64
+}
+
+func (g GPU) String() string {
+	return fmt.Sprintf("GPU Usage: %.2f\nVRAM Usage: %.2f\nVRAM Total: %.2f\nVRAM Used: %.2f\nVRAM Available: %.2f",
+		g.gpuBusyPercent, g.vramBusyPercent, g.vramTotal, g.vramUsed, g.vramAvailable)
+}
+
+func (g *GPU) UpdateGPUReading() {
+
+}
+
+// going to have to get data from multiple files. maybe create and return a map?
+// might be more performant to just return a [][]byte and know for myself
+// what index means for each
+func getGPUInfoData() {
+
+}
